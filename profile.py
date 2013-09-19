4f6a6002500900a764718e41037a709d2bc90981
Amit K Rao <amitraok@fastmail.in>
2013-09-18 22:01:49 -0700

    initial revision

---
+++ profile.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/about")
+def about():
+	return render_template("about.html",
+		title = "About Me")
+
+@app.route("/skills")
+def skills():
+	skills = {
+		'Platforms' : ['IBM Mainframe - z/OS/MVS', 'Windows', 'Mac OS', 'Linux'],
+		'Databases' : ['IMS', 'DB2', 'Teradata', 'MySql'],
+		'Others' : ['Python', 'HTML/CSS', 'SAS', 'REXX', 'COBOL']
+	}
+	return render_template("skills.html",
+		title = "Skills",
+		skills = skills)
+
+@app.route("/contact")
+def contact():
+	return render_template("contact.html",
+		title = "Contact Me")
+
+if __name__ == '__main__':
+	app.run(debug=True)
